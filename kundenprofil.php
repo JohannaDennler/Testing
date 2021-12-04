1650b2308fed8b5bf763158623c5e4f464fc8e63
Johanna <dennle01@ads.uni-passau.de>
2021-12-04 15:55:44 +0100

    First test

---
+++ kundenprofil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Hello World</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <li> <a href="index.html">Home</a> </li>
+      <li> <a href="..">Zurück</a> </li>
+      <li> <a href="#">Logout</a> </li>
+    </ul>
+  </nav>
+
+  <h1>Kundenprofil</h1>
+  <p>Name: Johanna Dennler</p>
+  <p>Email: Johanna@Dennler.de</p>
+  <p>Adresse: Kleine Messergasse 16
+              94032 Passau
+              Deutschland</p>
+  <p>Info: Für Profiländerungen eine Email an ... schicken.</p>
+</body>
+
+</html>
